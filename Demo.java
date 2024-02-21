3014d2d45817da17845b49a1a8002e685cc10cf1
UdaynaDash21 <dashudayan2@gmail.com>
2024-02-21 08:03:30 +0530

    Initial Commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+
+ public Demo(){
+    System.err.println("Constructor Created");
+ }
+
+    public static void main(String[] args) {
+         System.out.println("Initial Changes..");
+    }
+}
